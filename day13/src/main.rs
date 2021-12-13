fd9d267876df2a1f73c8b0c0c64df92bed7c4d86
3jörn Mårtensson <bearmarshal@gmail.com>
2021-12-13 17:36:40 +0100

    Dag 13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -79,5 +79,73 @@ fn part1(input: &str) {
 }
 
 fn part2(input: &str) {
-    println!("Part 2: {}", "");
+    lazy_static! {
+        static ref DOT_REGEX: Regex = Regex::new(r"(?P<x>\d+),(?P<y>\d+)").unwrap();
+        static ref FOLD_REGEX: Regex =
+            Regex::new(r"fold along (?P<axis>[xy])=(?P<coordinate>\d+)").unwrap();
+    }
+    let (dots, folds) = input.split_once("\n\n").unwrap();
+
+    let mut paper = DOT_REGEX
+        .captures_iter(dots)
+        .map(|capture| {
+            (
+                capture.name("y").unwrap().as_str().parse().unwrap(),
+                capture.name("x").unwrap().as_str().parse().unwrap(),
+            )
+        })
+        .collect::<HashSet<(usize, usize)>>();
+
+    for capture in FOLD_REGEX.captures_iter(folds) {
+        let coordinate = capture
+            .name("coordinate")
+            .unwrap()
+            .as_str()
+            .parse()
+            .unwrap();
+        match capture.name("axis").unwrap().as_str() {
+            "y" => {
+                paper = paper
+                    .into_iter()
+                    .filter_map(|(y, x)| {
+                        if y < coordinate {
+                            Some((y, x))
+                        } else if y > coordinate {
+                            Some((2 * coordinate - y, x))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect()
+            }
+            "x" => {
+                paper = paper
+                    .into_iter()
+                    .filter_map(|(y, x)| {
+                        if x < coordinate {
+                            Some((y, x))
+                        } else if x > coordinate {
+                            Some((y, 2 * coordinate - x))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect()
+            }
+            _ => panic!(),
+        }
+    }
+
+    let mut y_max = 0;
+    let mut x_max = 0;
+    for (y, x) in &paper {
+        y_max = usize::max(y_max, *y);
+        x_max = usize::max(x_max, *x);
+    }
+    let mut paper_grid = vec![vec![' ' as u8; x_max + 1]; y_max + 1];
+    for (y, x) in paper {
+        paper_grid[y][x] = '#' as u8;
+    }
+
+    println!("Part 2: {}", paper_grid.into_iter().fold(String::from(""), move |previous, line| previous + "\n" + &String::from_utf8(line).unwrap()))
 }
